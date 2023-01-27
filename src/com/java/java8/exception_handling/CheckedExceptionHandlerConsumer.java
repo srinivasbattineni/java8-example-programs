b49c631cbf0780ace10a6c09ef484452ffe6c415
Srinivas Battineni <srinivasbattineni9@gmail.com>
2023-01-27 18:25:18 +0530

    java8 example programs

---
+++ src/com/java/java8/exception_handling/CheckedExceptionHandlerConsumer.java
@@ -0,0 +1,7 @@
+package com.java.java8.exception_handling;
+
+@FunctionalInterface
+public interface CheckedExceptionHandlerConsumer<Target,ExObj extends Exception> {
+
+    public void accept(Target target) throws ExObj;
+}
